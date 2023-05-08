d1cbc13e1112888ccc69da349b1f0d806cefa006
jackfiled <xcrenchangjun@outlook.com>
2023-05-08 19:29:15 +0800

    add: 添加libuv依赖库

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
-#include "stdio.h"
+#include "stdlib.h"
+#include "uv.h"
 
 int main()
 {
-    printf("Hello, world!\n");
+    uv_loop_t* loop = malloc(sizeof(uv_loop_t));
+    uv_loop_init(loop);
+
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    // QUIT
+    uv_loop_close(loop);
+    free(loop);
+
     return 0;
 }
