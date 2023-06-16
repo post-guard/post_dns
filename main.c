c2aa076b45d0b426ad41a56731e29abea8197277
jackfiled <xcrenchangjun@outlook.com>
2023-06-16 15:31:12 +0800

    add: udp 示例程序

--- main.c
+++ main.c
@@ -2,17 +2,46 @@
 #include "uv.h"
 #include "stdio.h"
 
+uv_udp_t bind_socket;
+
+static void udp_alloc_buffer(uv_handle_t *handle, size_t suggest_size, uv_buf_t *buf)
+{
+    buf->base = malloc(suggest_size);
+    buf->len = suggest_size;
+}
+
+static void udp_on_read(
+        uv_udp_t *request, ssize_t number, const uv_buf_t *buf, const struct sockaddr *address, unsigned flags)
+{
+    if (number < 0)
+    {
+        return;
+    }
+
+    // 获得发送方地址
+    // 这里获得的buf长度有问题 是65535
+    char sender[17] = {};
+    uv_ip4_name((struct sockaddr_in *)address, sender, 16);
+    printf("Receive data from %s\n", sender);
+    printf("Data length: %zu\n", buf->len);
+    printf("Data: ");
+    for (int i = 0; i < buf->len; i++)
+    {
+        printf("%d", buf->base[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
-    uv_loop_t* loop = malloc(sizeof(uv_loop_t));
-    uv_loop_init(loop);
+    uv_loop_t *loop = uv_default_loop();
 
-    uv_run(loop, UV_RUN_DEFAULT);
+    uv_udp_init(loop, &bind_socket);
 
-    // QUIT
-    uv_loop_close(loop);
-    free(loop);
+    struct sockaddr_in bind_address;
+    uv_ip4_addr("127.0.0.1", 53, &bind_address);
+    uv_udp_bind(&bind_socket, (const struct sockaddr *)&bind_address, UV_UDP_REUSEADDR);
+    uv_udp_recv_start(&bind_socket, udp_alloc_buffer, udp_on_read);
 
-    printf("Hello, libuv!\n");
-    return 0;
+    return uv_run(loop, UV_RUN_DEFAULT);
 }
