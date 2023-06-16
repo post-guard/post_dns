f933c795283fa8e51996c987ce5dc97bfce9f43f
jackfiled <xcrenchangjun@outlook.com>
2023-06-16 13:39:42 +0800

    add: boolean枚举定义

---
+++ include/boolean.h
@@ -0,0 +1,13 @@
+//
+// Created by ricardo on 23-6-16.
+//
+
+#ifndef POST_DNS_BOOLEAN_H
+#define POST_DNS_BOOLEAN_H
+
+typedef enum {
+    false,
+    true
+} boolean;
+
+#endif //POST_DNS_BOOLEAN_H
